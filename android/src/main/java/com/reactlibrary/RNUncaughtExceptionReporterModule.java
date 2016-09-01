eb638918b07e2f5cab3cd2c7dfe9d18b2af52a28
npomfret <nick-github@snowmonkey.co.uk>
2016-09-01 12:52:44 +0100

    implemented android version

--- android/src/main/java/com/reactlibrary/RNUncaughtExceptionReporterModule.java
+++ android/src/main/java/com/reactlibrary/RNUncaughtExceptionReporterModule.java
@@ -1,10 +1,18 @@
 
 package com.reactlibrary;
 
+import android.util.Log;
+
+import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import static java.lang.Thread.getDefaultUncaughtExceptionHandler;
+import static java.lang.Thread.setDefaultUncaughtExceptionHandler;
 
 public class RNUncaughtExceptionReporterModule extends ReactContextBaseJavaModule {
 
@@ -16,8 +24,27 @@ public class RNUncaughtExceptionReporterModule extends ReactContextBaseJavaModul
     }
 
     @ReactMethod
-    public void listen(Callback callback) {
-        callback.invoke("hello from android");
+    public void listenToErrors(final Callback callback) {
+        final Thread.UncaughtExceptionHandler exceptionHandler = getDefaultUncaughtExceptionHandler();
+
+        setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread thread, Throwable ex) {
+                try {
+                    Log.e("UncaughtException", "UncaughtException on " + thread, ex);
+
+                    StringWriter errors = new StringWriter();
+                    ex.printStackTrace(new PrintWriter(errors));
+                    String text = errors.toString();
+
+                    callback.invoke(text);
+                } catch (Exception e) {
+                    Log.e("UncaughtException", "Failed to report error", e);
+                } finally {
+                    exceptionHandler.uncaughtException(thread, ex);
+                }
+            }
+        });
     }
 
     @Override
